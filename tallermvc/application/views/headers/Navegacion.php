9c5395f6be36225328d19255da3068449798da39
Gianfranco Lagomarsino <gianlago@hotmail.com>
2015-05-06 21:26:33 -0400

    probando
    actualizando

---
+++ tallermvc/application/views/headers/Navegacion.php
@@ -0,0 +1,36 @@
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Taller</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"> <a href="<?php echo base_url('index.php'); ?>">Inicio</a>< /li>
+            <li> <a href="<?php echo base_url('index.php/productos/administracion'); ?>">Administracion Productos</a> </li>
+            <li> <a href="<?php echo base_url('index.php/productos/administracion'); ?>">Administracion Servicios</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	<br></br>
+	<br></br>
+	
+</body>
